7236b90223c77f6b96040f1fe19d2785e68b479e
ThunderGod77 <kshitijgang76@gmail.com>
2023-06-21 02:15:20 +0530

    added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +19,13 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+
   ],
   markdown: {
     remarkPlugins: [
